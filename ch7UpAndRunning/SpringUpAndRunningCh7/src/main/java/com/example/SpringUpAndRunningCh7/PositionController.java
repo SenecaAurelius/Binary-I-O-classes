926bf218bb19151eba5af5443230de85607f1afe
Earl <aurelius.confucius@gmail.com>
2023-06-15 08:55:17 -0500

    EventHandler

---
+++ ch7UpAndRunning/SpringUpAndRunningCh7/src/main/java/com/example/SpringUpAndRunningCh7/PositionController.java
@@ -0,0 +1,30 @@
+package com.example.SpringUpAndRunningCh7;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class PositionController {
+    @NonNull
+    private final AircraftRepository repository;
+    private WebClient client = WebClient.create("http://localhost:7634/aircraft");
+
+    @GetMapping("/aircraft")
+    public String getCurrentAircraftPositions(Model model) {
+        repository.deleteAll();
+
+        client.get()
+                .retrieve()
+                .bodyToFlux(Aircraft.class)
+                .filter(plane -> !plane.getReg().isEmpty())
+                .toStream()
+                .forEach(repository::save);
+
+        model.addAttribute("currentPositions", repository.findAll());
+        return "positions";
+    }
+}
